413667a4fcdf1b1c54ceacb97d7118b23d61d720
Louis Morando <louis.morando@epita.fr>
2021-05-17 16:12:00 +0200

    Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,8 +4,23 @@ public class Launcher {
         final var scanner = new java.util.Scanner(System.in);
         var input = scanner.nextLine();
         do {
-            System.out.println("Unknown command");
+            if ("fibo".equals(input)) {
+                System.out.println("Veuillez entrer un nombre.");
+                var number = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(fibo(number));
+            }
+            else {
+                System.out.println("Unknown command");
+            }
             input = scanner.nextLine();
         } while (!"quit".equals(input));
+    }
+
+    public static int fibo(int number) {
+        if (number < 2) {
+            return number;
+        }
+        return fibo(number - 1) + fibo(number - 2);
     }
 }
