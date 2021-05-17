eb1883d64fdccd816c152104390fa1ebfee0620d
Louis Morando <louis.morando@epita.fr>
2021-05-17 15:57:33 +0200

    Launcher with while

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,9 +2,10 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("|---| Welcome |---|");
         final var scanner = new java.util.Scanner(System.in);
-        final String input = scanner.nextLine();
-        if (!"quit".equals(input)) {
+        var input = scanner.nextLine();
+        do {
             System.out.println("Unknown command");
-        }
+            input = scanner.nextLine();
+        } while (!"quit".equals(input));
     }
 }
